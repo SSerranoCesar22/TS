6598c44045a3ae451934798c33c680624d99e7ae
SergioSerranoo <sergioserranocesar@gmail.comm>
2023-10-14 12:58:30 +0200

    second update of my calculator

--- Calculator/inputFilesTS/main.ts
+++ Calculator/inputFilesTS/main.ts
@@ -1,134 +1,101 @@
 const scr = document.getElementById('screen') as HTMLElement;
 const numberButtons = document.querySelectorAll(".number");
+const decimalButton = document.querySelector(".decimal");
 const clear = document.getElementById('reset') as HTMLElement;
 const del = document.getElementById('del') as HTMLElement;
 const scrDel = document.querySelectorAll("#screen p");
-let isSecondValue = false;
-let isNegative = false;
-let value1: string[] = [];
-let value2: string[] = [];
+let expression = "";
+let lastResult = "";
+
 document.addEventListener("DOMContentLoaded", function () {
     inputScreen();
 });
+
+function inputScreen() {
+    numberButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            if (lastResult !== "") {
+                expression = "";
+                lastResult = "";
+            }
+            const value = button.getAttribute("data-value") || "";
+            expression += value;
+            updateScreen();
+        });
+    });
+
+    decimalButton?.addEventListener("click", () => {
+        if (lastResult !== "") {
+            expression = "";
+            lastResult = "";
+        }
+        if (!expression.endsWith(".")) {
+            expression += ".";
+            updateScreen();
+        }
+    });
+}
+
+function setSymbol(symbol: string) {
+    if (lastResult !== "") {
+        lastResult = "";
+    }
+    if (symbol === "X") {
+        expression += " * ";
+    } else {
+        expression += ` ${symbol} `;
+    }
+    updateScreen();
+}
+
+function updateScreen() {
+    const paragraph = scr.querySelector("p");
+    if (paragraph) {
+        paragraph.textContent = expression;
+    }
+}
+
+function cleanScreen() {
+    expression = "";
+    lastResult = "";
+    updateScreen();
+}
+
+function operation() {
+    const resultParagraph = scr.querySelector("p");
+
+    if (resultParagraph) {
+        try {
+            const sanitizedExpression = expression.replace(/X/g, '*'); 
+            const result = eval(sanitizedExpression);
+            if (isFinite(result)) {
+                resultParagraph.textContent = result.toString();
+                lastResult = result.toString();
+            } else {
+                resultParagraph.textContent = "Error";
+                lastResult = "Error";
+            }
+        } catch (error) {
+            resultParagraph.textContent = "Error";
+            lastResult = "Error";
+        }
+    }
+}
+
 document.getElementById('add')?.addEventListener("click", () => setSymbol("+"));
 document.getElementById('subtract')?.addEventListener("click", () => setSymbol("-"));
 document.getElementById('divide')?.addEventListener("click", () => setSymbol("/"));
 document.getElementById('multiply')?.addEventListener("click", () => setSymbol("X"));
+document.getElementById('reset')?.addEventListener("click", cleanScreen);
+document.getElementById('del')?.addEventListener("click", deleteLastCharacter);
+document.getElementById('equal')?.addEventListener("click", operation);
+
 function deleteLastCharacter() {
-    const paragraph = scr.querySelector("p");
-    if (paragraph) {
-        const currentContent = paragraph.textContent || "";
-        if (currentContent.length > 0) {
-            paragraph.textContent = currentContent.slice(0, -1);
-            if (isSecondValue) {
-                value2.pop();
-            } else {
-                value1.pop();
-            }
-        }
+    if (lastResult !== "") {
+        lastResult = "";
+    }
+    if (expression.length > 0) {
+        expression = expression.slice(0, -1);
+        updateScreen();
     }
 }
-
-
-function setSymbol(symbol: string) {
-    const paragraph = scr.querySelector("p");
-    if (paragraph) {
-        const currentContent = paragraph.textContent || "";
-
-        if (currentContent.length === 0) {
-            // Controla el caso de un número negativo
-            if (symbol === "-") {
-                paragraph.textContent = "-";
-            }
-        } else {
-            paragraph.textContent = currentContent + symbol;
-            isSecondValue = true; // Indica que estamos ingresando el segundo valor
-        }
-    }
-}
-function num1(number: number) {
-    value1.push(number.toString());
-}
-
-function num2(number: number) {
-    value2.push(number.toString());
-}
-function inputScreen() {
-    numberButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const value = button.getAttribute("data-value") ?? "";
-            const paragraph = scr.querySelector("p");
-
-            if (paragraph) {
-                const currentContent = paragraph.textContent || "";
-                const parsedValue = parseFloat(value);
-
-                if (!isNaN(parsedValue)) {
-                    paragraph.textContent = currentContent + parsedValue;
-
-                    if (isNegative) {
-                        num2(parsedValue);
-                    } else {
-                        num1(parsedValue);
-                    }
-                }
-            }
-        });
-    });
-}
-function cleanScreen() {
-    for (const paragraph of scrDel) {
-        paragraph.textContent = "";
-    }
-}
-
-
-function concatenateAndParse(value1: string[], value2: string[]) : [number, number] {
-    const concatenatedValue = parseFloat(value1.join(''));
-    const concatenatedValue2 = parseFloat(value2.join(''));
-    return[concatenatedValue,concatenatedValue2];
-}
-function sum(value1: string[], value2 : string[]) :number{
-    const [num1,num2] = concatenateAndParse(value1,value2);
-    const result = num1 + num2;
-    return result;
-}
-function sub(value1: string[], value2 : string[]) :number{
-    const [num1,num2] = concatenateAndParse(value1,value2);
-    const  result = num1 - num2;
-    return result;
-}
-function div(value1: string[], value2 : string[]) :number | string{
-    const [num1,num2] = concatenateAndParse(value1,value2);
-    let result;
-    result = num1 / num2;
-    if(num2 == 0){
-        result = "math error";
-    }
-    return result;
-}
-function multp(value1: string[], value2 : string[]) :number{
-    const [num1,num2] = concatenateAndParse(value1,value2);
-    const  result = num1 * num2;
-    return result;
-}
-function operation() {
-    const resultParagraph = scr.querySelector("p")!;
-
-    if (value1.length === 0 || value2.length === 0) {
-        resultParagraph.textContent = "Ingrese números en ambos campos";
-    } else {
-        const operationSymbol = resultParagraph.textContent;  // Obtén el símbolo del contenido del párrafo
-
-        if (operationSymbol === "+") {
-            resultParagraph.textContent = sum(value1, value2).toString();
-        } else if (operationSymbol === "-") {
-            resultParagraph.textContent = sub(value1, value2).toString();
-        } else if (operationSymbol === "X") {
-            resultParagraph.textContent = multp(value1, value2).toString();
-        } else if (operationSymbol === "÷") {
-            resultParagraph.textContent = div(value1, value2).toString();
-        }
-    }
-}
-
--- Calculator/outputFilesJS/main.js
+++ Calculator/outputFilesJS/main.js
@@ -1,132 +1,96 @@
 "use strict";
-var _a, _b, _c, _d;
+var _a, _b, _c, _d, _e, _f, _g;
 const scr = document.getElementById('screen');
 const numberButtons = document.querySelectorAll(".number");
+const decimalButton = document.querySelector(".decimal");
 const clear = document.getElementById('reset');
 const del = document.getElementById('del');
 const scrDel = document.querySelectorAll("#screen p");
-let isSecondValue = false;
-let isNegative = false;
-let value1 = [];
-let value2 = [];
+let expression = "";
+let lastResult = "";
 document.addEventListener("DOMContentLoaded", function () {
     inputScreen();
 });
+function inputScreen() {
+    numberButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            if (lastResult !== "") {
+                expression = "";
+                lastResult = "";
+            }
+            const value = button.getAttribute("data-value") || "";
+            expression += value;
+            updateScreen();
+        });
+    });
+    decimalButton === null || decimalButton === void 0 ? void 0 : decimalButton.addEventListener("click", () => {
+        if (lastResult !== "") {
+            expression = "";
+            lastResult = "";
+        }
+        if (!expression.endsWith(".")) {
+            expression += ".";
+            updateScreen();
+        }
+    });
+}
+function setSymbol(symbol) {
+    if (lastResult !== "") {
+        lastResult = "";
+    }
+    if (symbol === "X") {
+        expression += " * ";
+    }
+    else {
+        expression += ` ${symbol} `;
+    }
+    updateScreen();
+}
+function updateScreen() {
+    const paragraph = scr.querySelector("p");
+    if (paragraph) {
+        paragraph.textContent = expression;
+    }
+}
+function cleanScreen() {
+    expression = "";
+    lastResult = "";
+    updateScreen();
+}
+function operation() {
+    const resultParagraph = scr.querySelector("p");
+    if (resultParagraph) {
+        try {
+            const sanitizedExpression = expression.replace(/X/g, '*');
+            const result = eval(sanitizedExpression);
+            if (isFinite(result)) {
+                resultParagraph.textContent = result.toString();
+                lastResult = result.toString();
+            }
+            else {
+                resultParagraph.textContent = "Error";
+                lastResult = "Error";
+            }
+        }
+        catch (error) {
+            resultParagraph.textContent = "Error";
+            lastResult = "Error";
+        }
+    }
+}
 (_a = document.getElementById('add')) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => setSymbol("+"));
 (_b = document.getElementById('subtract')) === null || _b === void 0 ? void 0 : _b.addEventListener("click", () => setSymbol("-"));
 (_c = document.getElementById('divide')) === null || _c === void 0 ? void 0 : _c.addEventListener("click", () => setSymbol("/"));
 (_d = document.getElementById('multiply')) === null || _d === void 0 ? void 0 : _d.addEventListener("click", () => setSymbol("X"));
+(_e = document.getElementById('reset')) === null || _e === void 0 ? void 0 : _e.addEventListener("click", cleanScreen);
+(_f = document.getElementById('del')) === null || _f === void 0 ? void 0 : _f.addEventListener("click", deleteLastCharacter);
+(_g = document.getElementById('equal')) === null || _g === void 0 ? void 0 : _g.addEventListener("click", operation);
 function deleteLastCharacter() {
-    const paragraph = scr.querySelector("p");
-    if (paragraph) {
-        const currentContent = paragraph.textContent || "";
-        if (currentContent.length > 0) {
-            paragraph.textContent = currentContent.slice(0, -1);
-            if (isSecondValue) {
-                value2.pop();
-            }
-            else {
-                value1.pop();
-            }
-        }
+    if (lastResult !== "") {
+        lastResult = "";
     }
-}
-function setSymbol(symbol) {
-    const paragraph = scr.querySelector("p");
-    if (paragraph) {
-        const currentContent = paragraph.textContent || "";
-        if (currentContent.length === 0) {
-            // Controla el caso de un número negativo
-            if (symbol === "-") {
-                paragraph.textContent = "-";
-            }
-        }
-        else {
-            paragraph.textContent = currentContent + symbol;
-            isSecondValue = true; // Indica que estamos ingresando el segundo valor
-        }
-    }
-}
-function num1(number) {
-    value1.push(number.toString());
-}
-function num2(number) {
-    value2.push(number.toString());
-}
-function inputScreen() {
-    numberButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            var _a;
-            const value = (_a = button.getAttribute("data-value")) !== null && _a !== void 0 ? _a : "";
-            const paragraph = scr.querySelector("p");
-            if (paragraph) {
-                const currentContent = paragraph.textContent || "";
-                const parsedValue = parseFloat(value);
-                if (!isNaN(parsedValue)) {
-                    paragraph.textContent = currentContent + parsedValue;
-                    if (isNegative) {
-                        num2(parsedValue);
-                    }
-                    else {
-                        num1(parsedValue);
-                    }
-                }
-            }
-        });
-    });
-}
-function cleanScreen() {
-    for (const paragraph of scrDel) {
-        paragraph.textContent = "";
-    }
-}
-function concatenateAndParse(value1, value2) {
-    const concatenatedValue = parseFloat(value1.join(''));
-    const concatenatedValue2 = parseFloat(value2.join(''));
-    return [concatenatedValue, concatenatedValue2];
-}
-function sum(value1, value2) {
-    const [num1, num2] = concatenateAndParse(value1, value2);
-    const result = num1 + num2;
-    return result;
-}
-function sub(value1, value2) {
-    const [num1, num2] = concatenateAndParse(value1, value2);
-    const result = num1 - num2;
-    return result;
-}
-function div(value1, value2) {
-    const [num1, num2] = concatenateAndParse(value1, value2);
-    let result;
-    result = num1 / num2;
-    if (num2 == 0) {
-        result = "math error";
-    }
-    return result;
-}
-function multp(value1, value2) {
-    const [num1, num2] = concatenateAndParse(value1, value2);
-    const result = num1 * num2;
-    return result;
-}
-function operation() {
-    const resultParagraph = scr.querySelector("p");
-    if (value1.length === 0 || value2.length === 0) {
-        resultParagraph.textContent = "Ingrese números en ambos campos";
-    }
-    else {
-        const operationSymbol = resultParagraph.textContent; // Obtén el símbolo del contenido del párrafo
-        if (operationSymbol === "+") {
-            resultParagraph.textContent = sum(value1, value2).toString();
-        }
-        else if (operationSymbol === "-") {
-            resultParagraph.textContent = sub(value1, value2).toString();
-        }
-        else if (operationSymbol === "X") {
-            resultParagraph.textContent = multp(value1, value2).toString();
-        }
-        else if (operationSymbol === "÷") {
-            resultParagraph.textContent = div(value1, value2).toString();
-        }
+    if (expression.length > 0) {
+        expression = expression.slice(0, -1);
+        updateScreen();
     }
 }
