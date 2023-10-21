e357df7b187737b3fa4a2127e3404029af9cd9ae
SergioSerranoo <sergioserranocesar@gmail.comm>
2023-10-21 18:44:10 +0200

    git oop ligth

--- ligth/outputFilesJS/main.js
+++ ligth/outputFilesJS/main.js
@@ -1,28 +1,45 @@
 "use strict";
-const switchButton = document.getElementById('buttonSwitch');
-const ligthImage = document.querySelector('#ligth img');
-const zoomRange = document.getElementById('range');
-const rangeInput2 = document.getElementById("range2");
-const valueZoom = document.getElementById('value');
-function toggleLight(event) {
-    if (ligthImage.src.includes("off.jpg")) {
-        ligthImage.src = "images/on.jpg";
-        switchButton.innerHTML = '<img src="images/bon.jpg" alt="switchOn">';
+class Ligth {
+    constructor() {
+        this.switchButton = document.getElementById("buttonSwitch");
+        this.ligthImage = document.querySelector("#ligth img");
+        this.zoomRange = document.getElementById("range");
+        this.rangeInput2 = document.getElementById("range2");
+        this.rotateImage = document.getElementById("range3");
+        this.switchButton.addEventListener("click", this.toggleLight.bind(this));
+        this.rangeInput2.addEventListener("input", this.updateOpacity.bind(this));
+        this.zoomRange.addEventListener("input", this.zoom.bind(this));
+        this.rotateImage.addEventListener("input", this.rotate.bind(this));
     }
-    else {
-        ligthImage.src = "images/off.jpg";
-        switchButton.innerHTML = '<img src="images/boff.jpg" alt="switchOff">';
+    toggleLight() {
+        if (this.ligthImage.src.includes("on.jpg")) {
+            this.ligthImage.src = "images/off.jpg";
+            this.switchButton.innerHTML = '<img src="images/boff.jpg" alt="switchOff">';
+        }
+        else {
+            this.ligthImage.src = "images/on.jpg";
+            this.switchButton.innerHTML = '<img src="images/bon.jpg" alt="switchOn">';
+        }
+    }
+    updateOpacity() {
+        const value = parseFloat(this.rangeInput2.value);
+        const opacity = value / 100;
+        this.ligthImage.style.opacity = opacity.toString();
+    }
+    zoom() {
+        const scaleValue = this.zoomRange.value;
+        this.ligthImage.style.transform = `scale(${scaleValue})`;
+    }
+    rotate() {
+        const rotateValue = parseInt(this.rotateImage.value, 10);
+        const maxRotation = 50;
+        let rotationAngle = (rotateValue / maxRotation) * 360;
+        if (rotateValue < maxRotation) {
+            rotationAngle = (1 - rotateValue / maxRotation) * 360;
+        }
+        this.ligthImage.style.transform = `rotate(${rotationAngle}deg)`;
     }
 }
-function updateOpacity() {
-    const value = parseFloat(rangeInput2.value);
-    const opacity = value / 100;
-    ligthImage.style.opacity = opacity.toString();
-}
-function zoom() {
-    const scaleValue = zoomRange.value;
-    ligthImage.style.transform = `scale(${scaleValue})`;
-}
-switchButton.addEventListener("click", toggleLight);
-rangeInput2.addEventListener("input", updateOpacity);
-zoomRange.addEventListener('input', zoom);
+document.addEventListener("DOMContentLoaded", function () {
+    const ligth = new Ligth();
+});
