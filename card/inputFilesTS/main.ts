982d365ea6213ccf9128421efbeb2d6e74f52b07
SergioSerranoo <sergioserranocesar@gmail.comm>
2023-10-21 19:06:44 +0200

    update card app to poo

--- card/inputFilesTS/main.ts
+++ card/inputFilesTS/main.ts
@@ -1,18 +1,35 @@
-const cardContainer = document.getElementById("cards")!;
-const images = cardContainer.getElementsByTagName("img")!;
-function cambiarImagenFront(event: MouseEvent) {
-    const img = event.target as HTMLImageElement;
-    if (img && img.src) {
-        img.src = "images/card-front.png";
+class Card {
+    private cardContainer: HTMLElement;
+    private images: HTMLCollectionOf<HTMLImageElement>;
+
+    constructor() {
+        this.cardContainer = document.getElementById("cards") as HTMLElement;
+        this.images = this.cardContainer.getElementsByTagName("img");
+        this.attachEventListeners();
+    }
+
+    private changeImageByFront(event: MouseEvent) {
+        const img = event.target as HTMLImageElement;
+        if (img && img.src) {
+            img.src = "images/card-front.png";
+        }
+    }
+
+    private rchangeImageBack(event: MouseEvent) {
+        const img = event.target as HTMLImageElement;
+        if (img && img.src) {
+            img.src = "images/card-back.png";
+        }
+    }
+
+    private attachEventListeners() {
+        for (const img of this.images) {
+            img.addEventListener("mouseover", this.changeImageByFront);
+            img.addEventListener("mouseout", this.rchangeImageBack);
+        }
     }
 }
-function restaurarImagenBack(event: MouseEvent) {
-    const img = event.target as HTMLImageElement;
-    if (img && img.src) {
-        img.src = "images/card-back.png";
-    }
-}
-for (const img of images) {
-    img.addEventListener("mouseover", cambiarImagenFront);
-    img.addEventListener("mouseout", restaurarImagenBack);
-}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const card = new Card();
+});
